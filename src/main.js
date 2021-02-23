7ea5024ebf724a4fd4b51ccfd150b06903aecd96
Eric Rabun <errabun@gmail.com>
2021-02-22 20:12:16 -0700

    Create button event listeners; add idea script

---
+++ src/main.js
@@ -0,0 +1,8 @@
+var userInputTitle = document.querySelector('#userCardTitle');
+var userInputBody = document.querySelector('#userCardBody');
+
+var starredIdeaBtn = document.querySelector('starred-ideas-btn');
+var saveIdeaBtn = document.querySelector('save-idea-btn');
+
+saveIdeaBtn.addEventListener('click', )
+starredIdeaBtn.addEventListener('click', )
