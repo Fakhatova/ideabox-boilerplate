622c99ef2ea26eef2786376576284baba46164e4
Fara Akhatova <faraakhatova@gmail.com>
2021-02-24 14:02:33 -0700

    remove star parameter from class and access classes

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 var userInputTitle = document.querySelector('#userCardTitle');
 var userInputBody = document.querySelector('#userCardBody');
 
-var starredIdeaBtn = document.querySelector('starred-ideas-btn');
-var saveIdeaBtn = document.querySelector('save-idea-btn');
+var starredIdeaBtn = document.querySelector('.starred-ideas-btn');
+var saveIdeaBtn = document.querySelector('.save-idea-btn');
 
 saveIdeaBtn.addEventListener('click', createNewIdea);
 starredIdeaBtn.addEventListener('click', )
 
 function createNewIdea() {
-  var newIdeaCard = new Idea (userInputTitle.value, userInputBody.value, false);
-  return newIdeaCard; 
+  var newIdeaCard = new Idea(userInputTitle.value, userInputBody.value);
+  return newIdeaCard;
 }
