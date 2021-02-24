3b12b0b7a2f63bd75b586ac5c4f17f89e72b1e3d
Fara Akhatova <faraakhatova@gmail.com>
2021-02-24 14:22:05 -0700

    Create new idea card function

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ var userInputBody = document.querySelector('#userCardBody');
 
 var starredIdeaBtn = document.querySelector('.starred-ideas-btn');
 var saveIdeaBtn = document.querySelector('.save-idea-btn');
+var savedIdeaCards = [];
 
 saveIdeaBtn.addEventListener('click', createNewIdea);
-starredIdeaBtn.addEventListener('click', )
+// starredIdeaBtn.addEventListener('click', )
 
 function createNewIdea() {
+  event.preventDefault();
   var newIdeaCard = new Idea(userInputTitle.value, userInputBody.value);
   return newIdeaCard;
 }
