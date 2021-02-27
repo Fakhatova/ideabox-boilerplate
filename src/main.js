941e4848d11da7a4b4f38eb21297c395a7deb9d0
Eric Rabun <errabun@gmail.com>
2021-02-26 17:46:40 -0700

    Disabled save button with empty input fields

--- src/main.js
+++ src/main.js
@@ -1,31 +1,34 @@
-var userInputTitle = document.querySelector('#userCardTitle');
-var userInputBody = document.querySelector('#userCardBody');
+var userInputs = document.querySelectorAll('.user-input');
 var starredIdeaBtn = document.querySelector('.starred-ideas-btn');
 var saveIdeaBtn = document.querySelector('.save-idea-btn');
 var savedIdeaCards = [];
 var ideaForm = document.querySelector('.user-idea-form')
 var savedCardsGrid = document.querySelector('.saved-cards-grid')
-
+var userCardTitle = document.getElementById('userCardTitle');
+var userCardBody = document.getElementById('userCardBody');
 
 ideaForm.addEventListener('keydown', checkUserInputs);
 saveIdeaBtn.addEventListener('click', createNewIdea);
 
+saveIdeaBtn.disabled = true;
 
 function checkUserInputs() {
-  if (userInputTitle.value && userInputBody.value) {
-    saveIdeaBtn.classList.add('activate');
+  for (var i = 0; i < userInputs.length; i++) {
+    if (userInputs[i].value === '') {
+      saveIdeaBtn.disabled = true;
+    } else {
+      saveIdeaBtn.disabled = false;
+    }
   }
 }
 
 
 function createNewIdea() {
   event.preventDefault();
-  if (userInputTitle.value && userInputBody.value) {
-    var newIdeaCard = new Idea(userInputTitle.value, userInputBody.value);
-    addCard()
-    clearInputFields();
-    saveIdeaBtn.classList.remove('activate');
-  }
+  checkUserInputs();
+  var newIdeaCard = new Idea (userCardTitle.value, userCardBody.value);
+  addCard();
+  clearInputFields();
 }
 
 
@@ -37,8 +40,8 @@ function addCard() {
       <img class='delete-card-x' src="https://drive.google.com/uc?export=view&id=1DFdu572EVYb1SXhsXQ0XDqvfZ7prhJWg" alt="delete card x">
     </div>
     <article class='idea-title-body'>
-      <p class='idea-card-title'>${userInputTitle.value}</p>
-      <p class='idea-card-body'>${userInputBody.value}</p>
+      <p class='idea-card-title'>${userCardTitle.value}</p>
+      <p class='idea-card-body'>${userCardBody.value}</p>
     </article>
     <div class='comment-bar'>
       <img class='add-comment' src="https://drive.google.com/uc?export=view&id=1xk4FryiJY3UgKdzYQhKdKPBe75ubWaYt" alt="add comment">
@@ -50,6 +53,7 @@ function addCard() {
 
 
 function clearInputFields() {
-  userInputTitle.value = "";
-  userInputBody.value = "";
+  userCardTitle.value = "";
+  userCardBody.value = "";
+  saveIdeaBtn.disabled = true;
 }
