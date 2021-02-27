72ca3b07529a499adef6d330f2ca90f2c04d62e9
Sarah Lane <sarahlane8@gmail.com>
2021-02-27 08:39:41 -0700

    Add toggle to star feature and update Idea method

--- src/main.js
+++ src/main.js
@@ -5,13 +5,17 @@ var ideaForm = document.querySelector('.user-idea-form')
 var savedCardsGrid = document.querySelector('.saved-cards-grid')
 var userCardTitle = document.getElementById('userCardTitle');
 var userCardBody = document.getElementById('userCardBody');
+var starImageSrc = document.querySelector('.favorited-star');
 // var deleteIdeaCard = document.querySelector('.delete-card-x');
 
 ideaForm.addEventListener('keyup', checkUserInputs);
 saveIdeaBtn.addEventListener('click', createNewIdea);
-savedCardsGrid.addEventListener('click', deleteCard);
+savedCardsGrid.addEventListener('click', handleIdeaCardClick); //one function that would figure out what was clicked and then send you on to another fx
+// savedCardsGrid.addEventListener('click', changeStar);
 
 var savedIdeaCards = [];
+var whiteStar = "https://drive.google.com/uc?export=view&id=1TW-aKpR_uBW0Ayp6AtTqVq5cxuX27GiH";
+var redStar = "https://drive.google.com/uc?export=view&id=13_jn9vQvAdNzdcbdRmYoR6mBOZHoeqzU";
 saveIdeaBtn.disabled = true;
 
 function checkUserInputs() {
@@ -26,25 +30,33 @@ function createNewIdea() {
   clearInputFields();
 }
 
-
 function addCard(card) {
   savedIdeaCards.push(card);
-  savedCardsGrid.innerHTML += `
-    <section class="saved-cards" id="${card.id}">
-      <div class='favorite-delete'>
-        <img class='favorited-star' src="https://drive.google.com/uc?export=view&id=1TW-aKpR_uBW0Ayp6AtTqVq5cxuX27GiH" alt="favorite star">
-        <img class='delete-card-x' src="https://drive.google.com/uc?export=view&id=1DFdu572EVYb1SXhsXQ0XDqvfZ7prhJWg" alt="delete card x">
-      </div>
-      <article class='idea-title-body'>
-        <p class='idea-card-title'>${card.title}</p>
-        <p class='idea-card-body'>${card.body}</p>
-      </article>
-      <div class='comment-bar'>
-        <img class='add-comment' src="https://drive.google.com/uc?export=view&id=1xk4FryiJY3UgKdzYQhKdKPBe75ubWaYt" alt="add comment">
-        <span>Comment</span>
-      </div>
-    </section>
-  `
+  renderIdeaCards();
+}
+
+function renderIdeaCards() {
+  savedCardsGrid.innerHTML = '';
+  var ideaCardHtml = '';
+  for (var i = 0; i < savedIdeaCards.length; i++) {
+    ideaCardHtml += `
+      <section class="saved-cards" id="${savedIdeaCards[i].id}">
+        <div class='favorite-delete'>
+          <img class='favorited-star' src="${savedIdeaCards[i].star ? redStar: whiteStar}" alt="favorite star">
+          <img class='delete-card-x' src="https://drive.google.com/uc?export=view&id=1DFdu572EVYb1SXhsXQ0XDqvfZ7prhJWg" alt="delete card x">
+          </div>
+          <article class='idea-title-body'>
+            <p class='idea-card-title'>${savedIdeaCards[i].title}</p>
+            <p class='idea-card-body'>${savedIdeaCards[i].body}</p>
+          </article>
+        <div class='comment-bar'>
+          <img class='add-comment' src="https://drive.google.com/uc?export=view&id=1xk4FryiJY3UgKdzYQhKdKPBe75ubWaYt" alt="add comment">
+          <span>Comment</span>
+        </div>
+      </section>
+    `
+  }
+  savedCardsGrid.innerHTML = ideaCardHtml; //went from updating the Dom card# times to once
 }
 
 
@@ -55,8 +67,16 @@ function clearInputFields() {
 }
 
 
-function deleteCard(event) {
+function handleIdeaCardClick(event) {
   if (event.target.className === 'delete-card-x') {
+    deleteCard(event)
+  }
+  if (event.target.className === 'favorited-star') {
+    toggleIsFavorite(event);
+  }
+}
+
+function deleteCard(event) {
     var cardId = event.target.closest('.saved-cards').id;
     for (var i = 0; i < savedIdeaCards.length; i++) {
       if (savedIdeaCards[i].id === parseInt(cardId)) {
@@ -64,5 +84,17 @@ function deleteCard(event) {
       }
       event.target.closest('.saved-cards').remove();
     }
+  }
+
+
+function toggleIsFavorite(event) {
+  var cardId = event.target.closest('.saved-cards').id;
+  for (var i = 0; i < savedIdeaCards.length; i++) {
+    if (savedIdeaCards[i].id === parseInt(cardId)) {
+      // console.log(savedIdeaCards[i].star)
+      savedIdeaCards[i].updateIdea();
+
+    }
+    renderIdeaCards();
   }
 }
